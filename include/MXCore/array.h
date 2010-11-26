c44d4ce413d2554065c7167f0b8e5095d3526d31
Christoph Lang <mail.clang@gmail.com>
2010-11-26 17:39:22 +0100

    refactored array class hierarchy, added ObjectArrayTraits

--- include/MXCore/array.h
+++ include/MXCore/array.h
@@ -35,38 +35,95 @@
 
 namespace mxcore {
 
-// Array implementation for storing objects with finalizers. When getting and
-// setting items, proper alignment and offset are used. Finalizers as well as
-// objects are aligned, meaning that lots of memory is wasted for small objects.
-template <class MemberType>
-class FinalizerArrayTraits {
- public:
-  FinalizerArrayTraits(ScopeStack& scope, const size_t size) : scope_(scope) {
-    MemberType* item;
-    for (int32_t i = 0; i < size; ++i) {
-      item = scope_.AllocateWithFinalizer<MemberType>();
-      if (i == 0) {
-        items_ = item;
-      }
-    }
+namespace internal {
 
-    size_t finalizer_size = LinearAllocator::AlignSize(sizeof(Finalizer), 
-                                                       scope_.alignment());
-    size_t member_size = LinearAllocator::AlignSize(sizeof(MemberType),
-                                                    scope_.alignment());
-    stepsize_ = finalizer_size + member_size;
+// Base for all array classes.
+template <class MemberType>
+class ArrayBase {
+ public:
+  ArrayBase(ScopeStack& scope) : scope_(scope), items_(NULL) {
   }
 
   MX_FORCE_INLINE MemberType& GetItem(const int32_t index) {
-    uintptr_t base_address = reinterpret_cast<uintptr_t>(items_);
+    return items_[index];
+  }
+
+ protected:
+  ScopeStack& scope_;
+  MemberType* items_;
+};
+
+// Base class for array implementations that store their members sparsely in
+// memory (with gaps between actual items) due to alignment or prepended
+// finalizers. Derived classes have to initialize stepsize_ explicitly as this
+// constructor sets it to the illegal value -1.
+template <class MemberType>
+class SparseArrayBase : public ArrayBase<MemberType> {
+ public:
+  SparseArrayBase(ScopeStack& scope)
+      : ArrayBase<MemberType>(scope),
+        stepsize_(-1) {}
+
+  // Returns the item at the given index, using the step size to determine the
+  // proper memory address.
+  MX_FORCE_INLINE MemberType& GetItem(const int32_t index) {
+    uintptr_t base_address = reinterpret_cast<uintptr_t>(this->items_);
     uintptr_t item_address = base_address + (index * stepsize_);
     return *(reinterpret_cast<MemberType*>(item_address));
   }
 
- private:
-  ScopeStack& scope_;
-  MemberType* items_;
+ protected:
   size_t stepsize_;
+};
+
+}  // namespace internal
+
+// Array behavior for objects that do not require any teardown logic. Each
+// object is aligned to the scope's boundaries.
+template <class MemberType>
+class ObjectArrayTraits : public internal::SparseArrayBase<MemberType> {
+ public:
+  ObjectArrayTraits(ScopeStack& scope, const size_t size)
+      : internal::SparseArrayBase<MemberType>(scope) {
+    MemberType* item;
+
+    for (size_t i = 0; i < size; ++i) {
+      item = this->scope_.template AllocateObject<MemberType>();
+      if (i == 0) {
+        this->items_ = item;
+      }
+    }
+
+    this->stepsize_ = LinearAllocator::AlignSize(sizeof(MemberType),
+                                                 this->scope_.alignment());
+  }
+};
+
+// Array implementation for storing objects with finalizers. When getting and
+// setting items, proper alignment and offset are used. Finalizers as well as
+// objects are aligned, meaning that lots of memory is wasted for small objects.
+template <class MemberType>
+class FinalizerArrayTraits : public internal::SparseArrayBase<MemberType> {
+ public:
+  FinalizerArrayTraits(ScopeStack& scope, const size_t size)
+      : internal::SparseArrayBase<MemberType>(scope) {
+    MemberType* item;
+
+    for (size_t i = 0; i < size; ++i) {
+      // .template is needed to treat the following name as a dependent template
+      // name.
+      item = this->scope_.template AllocateWithFinalizer<MemberType>();
+      if (i == 0) {
+        this->items_ = item;
+      }
+    }
+
+    size_t finalizer_size = LinearAllocator::AlignSize(sizeof(Finalizer), 
+                                                       this->scope_.alignment());
+    size_t item_size = LinearAllocator::AlignSize(sizeof(MemberType),
+                                                  this->scope_.alignment());
+    this->stepsize_ = finalizer_size + item_size;
+  }
 };
 
 // Basic fixed-size array. The internal behavior of the array is defined by a
